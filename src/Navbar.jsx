d1f65f5cd626116ebf9924ac3ed5b35c22db636e
Ken Udoudo <kendo302003@gmail.com>
2023-12-25 09:50:31 -0800

    importing use state

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,10 @@
 import logo from './assests/logo.png'
 import hamburger from './assests/user-interface-burger.png'
+import { useState } from 'react';
 const Navbar = () => {
     return (
-        <div className='flex flex-shrink-0 justify-around items-center h-[84px] bg-[#F5F7FA]'>
-            <div className='lg:w-[154px] md:w-[120px]'><img src={ logo }/></div>
+        <div className='flex flex-shrink-0 justify-between px-[30px] md:px-0 md:justify-around items-center h-[84px] bg-[#F5F7FA]'>
+            <div className='lg:w-[154px] md:w-[120px] w-[100px]'><img src={ logo }/></div>
             <div className='hidden md:block'>
                 <ul className='inline-flex justify-center items-center lg:gap-[50px] md:gap-[20px] lg:text-[16px] md:text-[14px] cursor-pointer'>
                     <li>Home</li>
@@ -18,7 +19,7 @@ const Navbar = () => {
                 <p className='flex px-[20px] py-[10px] bg-[#F5F7FA] rounded-[6px] text-[14px] cursor-pointer'>Login</p>
                 <p className='flex px-[20px] py-[10px] bg-[#4CAF4F] rounded-[6px] text-[14px] text-white cursor-pointer'>Sign up</p>
             </div>
-            <div>
+            <div className='md:hidden'>
                 <img src={ hamburger }/>
             </div>
         </div>
