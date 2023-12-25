ceac874a2dce614500542db2597894534e4c5e82
Ken Udoudo <kendo302003@gmail.com>
2023-12-25 09:45:13 -0800

    responsive nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,11 @@
 import logo from './assests/logo.png'
+import hamburger from './assests/user-interface-burger.png'
 const Navbar = () => {
     return (
         <div className='flex flex-shrink-0 justify-around items-center h-[84px] bg-[#F5F7FA]'>
-            <div><img src={ logo }/></div>
-            <div>
-                <ul className='inline-flex justify-center items-center gap-[50px] text-[16px]'>
+            <div className='lg:w-[154px] md:w-[120px]'><img src={ logo }/></div>
+            <div className='hidden md:block'>
+                <ul className='inline-flex justify-center items-center lg:gap-[50px] md:gap-[20px] lg:text-[16px] md:text-[14px] cursor-pointer'>
                     <li>Home</li>
                     <li>Service</li>
                     <li>Feature</li>
@@ -13,9 +14,12 @@ const Navbar = () => {
                     <li>FAQ</li>
                 </ul>
             </div>
-            <div className='inline-flex items-start gap-[14px]'>
-                <p className='flex px-[20px] py-[10px] bg-[#F5F7FA] rounded-[6px] text-[14px]'>Login</p>
-                <p className='flex px-[20px] py-[10px] bg-[#4CAF4F] rounded-[6px] text-[14px] text-white'>Sign up</p>
+            <div className='hidden md:inline-flex items-start gap-[14px]'>
+                <p className='flex px-[20px] py-[10px] bg-[#F5F7FA] rounded-[6px] text-[14px] cursor-pointer'>Login</p>
+                <p className='flex px-[20px] py-[10px] bg-[#4CAF4F] rounded-[6px] text-[14px] text-white cursor-pointer'>Sign up</p>
+            </div>
+            <div>
+                <img src={ hamburger }/>
             </div>
         </div>
     );
