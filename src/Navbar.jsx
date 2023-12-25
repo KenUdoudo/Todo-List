05207783ef4dd611934d99d47417a73090339523
Ken Udoudo <kendo302003@gmail.com>
2023-12-25 10:05:33 -0800

    working

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,6 @@ import logo from './assests/logo.png'
 import hamburger from './assests/user-interface-burger.png'
 import { useState } from 'react';
 const Navbar = () => {
-    const HamburgerMenu = () => {
         const [isOpen, setIsOpen] = useState(false);
       
         const toggleMenu = () => {
@@ -29,8 +28,8 @@ const Navbar = () => {
                 <img src={ hamburger } onClick={ toggleMenu }/>
             </div>
             {isOpen && (
-                 <div className='absolute md:hidden'>
-                 <ul className='inline-flex justify-center items-center lg:gap-[50px] md:gap-[20px] lg:text-[16px] md:text-[14px] cursor-pointer'>
+                 <div className='absolute md:hidden top-[50px] right-0 mt-2 bg-white p-4'>
+                 <ul className='flex flex-col justify-center items-center lg:gap-[50px] md:gap-[20px] lg:text-[16px] md:text-[14px] cursor-pointer'>
                      <li>Home</li>
                      <li>Service</li>
                      <li>Feature</li>
@@ -38,6 +37,8 @@ const Navbar = () => {
                      <li>Testimonial</li>
                      <li>FAQ</li>
                  </ul>
+                 <p className='flex px-[20px] py-[10px] bg-[#F5F7FA] rounded-[6px] text-[14px] cursor-pointer'>Login</p>
+                <p className='flex px-[20px] py-[10px] bg-[#4CAF4F] rounded-[6px] text-[14px] text-white cursor-pointer'>Sign up</p>
              </div>
             )}
         </div>
