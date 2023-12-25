396fe40dbdaa60fe51acb7e2579bae04ce2321a8
Ken Udoudo <kendo302003@gmail.com>
2023-12-25 09:19:34 -0800

    nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,10 @@
 import logo from './assests/logo.png'
 const Navbar = () => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-shrink-0 justify-around items-center h-[84px] bg-[#F5F7FA]'>
             <div><img src={ logo }/></div>
             <div>
-                <ul>
+                <ul className='inline-flex justify-center items-center gap-[50px] text-[16px]'>
                     <li>Home</li>
                     <li>Service</li>
                     <li>Feature</li>
@@ -13,9 +13,9 @@ const Navbar = () => {
                     <li>FAQ</li>
                 </ul>
             </div>
-            <div>
-                <p>Login</p>
-                <p>Sign up</p>
+            <div className='inline-flex items-start gap-[14px]'>
+                <p className='flex px-[20px] py-[10px] bg-[#F5F7FA] rounded-[6px] text-[14px]'>Login</p>
+                <p className='flex px-[20px] py-[10px] bg-[#4CAF4F] rounded-[6px] text-[14px] text-white'>Sign up</p>
             </div>
         </div>
     );
