74380fa89abd61b5d95e1856f3650e17744bb00d
Ken Udoudo <kendo302003@gmail.com>
2023-12-26 08:42:29 -0800

    bg

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div>
         <Navbar />
 
-        <div className="flex flex-row px-[144px] py-[96px] items-center self-stretch gap-[104px]">
+        <div className="flex flex-row px-[144px] py-[96px] items-center self-stretch gap-[104px] bg-[#F5F7FA]">
             <div className="flex flex-col items-start gap-[32px]">
                 <h2 className="text-[64px] text-[#4D4D4D] font-semibold leading-[76px]">Lessons and insights <span className="text-[#4CAF4F]">from 8 years</span></h2>
                 <p className="text-[16px]">Where to grow your business as a photographer: site or social media?</p>
