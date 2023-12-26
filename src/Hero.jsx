3d28599748177b3f089f655d41f8018cbd0bf9ad
Ken Udoudo <kendo302003@gmail.com>
2023-12-26 08:40:47 -0800

    btn

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
         <Navbar />
 
         <div className="flex flex-row px-[144px] py-[96px] items-center self-stretch gap-[104px]">
-            <div>
-                <h2 className="text-[64px] text-[#4D4D4D] font-semibold">Lessons and insights <span className="text-[#4CAF4F]">from 8 years</span></h2>
+            <div className="flex flex-col items-start gap-[32px]">
+                <h2 className="text-[64px] text-[#4D4D4D] font-semibold leading-[76px]">Lessons and insights <span className="text-[#4CAF4F]">from 8 years</span></h2>
                 <p className="text-[16px]">Where to grow your business as a photographer: site or social media?</p>
-                <p className="px-[32px] py-[14px] bg-[#4CAF4F] ">Register</p>
+                <p className="px-[32px] py-[14px] bg-[#4CAF4F] text-white text-[16px] rounded-[4px] ">Register</p>
             </div>
             <div>
                 <img src={ heroimg }/>
