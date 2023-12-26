d2abf12e0fb46d49c0d3f73c97286c1d6f06c965
Ken Udoudo <kendo302003@gmail.com>
2023-12-26 09:46:11 -0800

    importing imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Hero from './Hero.jsx'
+import Section1 from './section1'
 function App() {
 
   return (
     <div>
      <Hero />
+
+     <Section1 />
     </div>
   )
 }
---
+++ src/section1.jsx
@@ -0,0 +1,15 @@
+const Section1 = () => {
+    return (
+        <div>
+            <div>
+                <h3>Our Clients</h3>
+                <p>We have been working with some Fortune 500+ clients</p>
+            </div>
+            <div>
+                <img src={}/>
+            </div>
+        </div>
+    );
+}
+ 
+export default Section1;
