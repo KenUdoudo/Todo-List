8d6e956209b53e0ee572330d2820b9913ff0d8a2
Ken Udoudo <kendo302003@gmail.com>
2023-12-27 09:20:43 -0800

    img

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './Hero.jsx'
 import Section1 from './section1'
+import Section2 from './section2.jsx'
 function App() {
 
   return (
@@ -8,6 +9,8 @@ function App() {
      <Hero />
 
      <Section1 />
+
+     <Section2 />
     </div>
   )
 }
---
+++ src/section2.jsx
@@ -0,0 +1,20 @@
+import mem from './assests/iconmem.png'
+const Section2 = () => {
+    return (
+        <div className="px-[40px]">
+            <div>
+                <h3>Manage your entire community in a single system</h3>
+                <p>Who is Nextcent suitable for?</p>
+            </div>
+
+            <div>
+                <div>
+                    <div><img src={ mem }/></div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+ 
+export default Section2;
