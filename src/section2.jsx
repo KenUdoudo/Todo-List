fadde55b7da9d28b5a38c67845ed1055a54ba345
Ken Udoudo <kendo302003@gmail.com>
2023-12-28 09:02:01 -0800

    css

--- src/section2.jsx
+++ src/section2.jsx
@@ -8,10 +8,22 @@ const Section2 = () => {
             </div>
 
             <div>
-                <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink'>
+                <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
                     <div><img src={ mem }/></div>
-                    <h3 className='text-[#4D4D4D]'>Membership Organisations</h3>
-                    <p>Our membership management software provides full automation of membership renewals and payments</p>
+                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
+                    <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
+                </div>
+
+                <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
+                    <div><img src={ mem }/></div>
+                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
+                    <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
+                </div>
+
+                <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
+                    <div><img src={ mem }/></div>
+                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
+                    <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
 
             </div>
