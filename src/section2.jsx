78c4e0e6310511413e54c49292803c2c9786c570
Ken Udoudo <kendo302003@gmail.com>
2023-12-28 09:04:32 -0800

    nat

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,4 +1,5 @@
 import mem from './assests/iconmem.png'
+import nat from './assests/icon-nat.png'
 const Section2 = () => {
     return (
         <div className="px-[40px]">
@@ -15,7 +16,7 @@ const Section2 = () => {
                 </div>
 
                 <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
-                    <div><img src={ mem }/></div>
+                    <div><img src={ nat }/></div>
                     <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
                     <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
