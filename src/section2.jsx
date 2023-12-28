7251a408dca0d4d3587bcfdd708a315bd6e0b21b
Ken Udoudo <kendo302003@gmail.com>
2023-12-28 09:08:38 -0800

    rename

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,5 +1,6 @@
 import mem from './assests/iconmem.png'
 import nat from './assests/icon-nat.png'
+import club from './assests/iconclub.png'
 const Section2 = () => {
     return (
         <div className="px-[40px]">
@@ -17,13 +18,13 @@ const Section2 = () => {
 
                 <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
                     <div><img src={ nat }/></div>
-                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
+                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>National Associations</h3>
                     <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
 
                 <div className='flex flex-col w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
-                    <div><img src={ mem }/></div>
-                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
+                    <div><img src={ club }/></div>
+                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Clubs And Groups</h3>
                     <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
 
