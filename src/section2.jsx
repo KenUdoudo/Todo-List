fac4455bddf7fee8163b70812a6cad5695a14fd4
Ken Udoudo <kendo302003@gmail.com>
2023-12-29 08:47:34 -0800

    done i think

--- src/section2.jsx
+++ src/section2.jsx
@@ -12,19 +12,19 @@ const Section2 = () => {
             <div className='flex flex-col md:flex-row px-[40px] lg:px-[100px] xl:px-[144px] justify-between items-center'>
                 <div className='flex flex-col w:-[190px] lg:w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
                     <div><img src={ mem }/></div>
-                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Membership Organisations</h3>
+                    <h3 className='text-[#4D4D4D] text-[20px] lg:text-[28px] font-[700]'>Membership Organisations</h3>
                     <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
 
                 <div className='flex flex-col w:-[190px] lg:w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
                     <div><img src={ nat }/></div>
-                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>National Associations</h3>
+                    <h3 className='text-[#4D4D4D] text-[20px] lg:text-[28px] font-[700]'>National Associations</h3>
                     <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
 
                 <div className='flex flex-col w:-[190px] lg:w-[299px] px-[32px] py-[24px] items-center gap-[8px] flex-shrink text-center'>
                     <div><img src={ club }/></div>
-                    <h3 className='text-[#4D4D4D] text-[28px] font-[700]'>Clubs And Groups</h3>
+                    <h3 className='text-[#4D4D4D] text-[20px] lg:text-[28px] font-[700]'>Clubs And Groups</h3>
                     <p className='text-[#717171] text-[14px] font-[400]'>Our membership management software provides full automation of membership renewals and payments</p>
                 </div>
 
