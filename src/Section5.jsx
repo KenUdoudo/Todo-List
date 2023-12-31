505cf4346bff622616ea0562059b7e43a80a187e
Ken Udoudo <kendo302003@gmail.com>
2023-12-31 01:00:47 -0800

    new section

---
+++ src/Section5.jsx
@@ -0,0 +1,17 @@
+import five from './assests/mobile-login-pana.png'
+const Section5 = () => {
+    return (
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center px-[40px] lg:px-[100px] xl:px-[144px] self-stretch font-inter">
+        <div className='px-[30px] md:px-0'><img src={ five }/></div>
+
+
+        <div className="flex flex-col items-center md:items-start gap-[10px] lg:gap-[20px] xl:gap-[32px] w-full md:w-3/5 lg:w-2/4">
+            <h2 className="px-[20px] md:px-0 text-[25px] md:text-[30px] lg:text-[36px] text-[#4D4D4D] font-semibold text-center md:text-left">How to design your site footer like we did</h2>
+            <p className="px-[20px] md:px-0 text-[12px] lg:text-[16px] text-[#717171] text-center md:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+            <p className="px-[25px] lg:px-[32px] py-[7px] lg:py-[14px] bg-[#4CAF4F] text-white text-[16px] rounded-[4px] ">Learn More</p>
+        </div>
+    </div>
+    );
+}
+ 
+export default Section5;
