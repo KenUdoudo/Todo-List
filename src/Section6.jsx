e3e1700b5562e2c4a8f2eb47e7fbac6a02344ee2
Ken Udoudo <kendo302003@gmail.com>
2023-12-31 01:38:13 -0800

    flex

--- src/Section6.jsx
+++ src/Section6.jsx
@@ -11,7 +11,7 @@ const Section6 = () => {
         <div className='flex flex-col-reverse md:flex-row justify-between items-center py-[20px] px-[40px] lg:px-[100px] xl:px-[144px] self-stretch font-inter gap-[78px] bg-[#F5F7FA]'>
             <div><img src={ tesla }/></div>
 
-            <div className="flex flex-col items-center md:items-start gap-[10px] lg:gap-[20px] xl:gap-[32px] w-full md:w-3/5 lg:w-3/4">
+            <div className="flex flex-col items-center md:items-start gap-[10px] w-full md:w-3/5 lg:w-3/4">
                 <p className='text-[#717171] text-[16px] font-[500] self-stretch'>Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.</p>
                 <p className='text-[#4CAF4F] text-[20px] font-[600] self-stretch'>Tim Smith</p>
                 <p className='text-[#89939E] text-[16px] font-[400] self-stretch'>British Dragon Boat Racing Association</p>
@@ -23,8 +23,8 @@ const Section6 = () => {
                 <div className='px-[10px] md:px-0'><img src={ four }/></div>
                 <div className='px-[10px] md:px-0'><img src={ five }/></div>
                 <div className='px-[10px] md:px-0'><img src={ six }/></div>
-                <div className='px-[10px] md:px-0'>
-                    <p>Meet all customers</p>
+                <div className='px-[10px] md:px-0 flex p-[8px] items-center gap-[8px] flex-1-0-0'>
+                    <p className='text-[#4CAF4F] text-[20px] font-[600]'>Meet all customers</p>
                     <div><img src={ arrow }/></div>
                 </div>
             </div>
