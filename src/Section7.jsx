b976dccc69acda9ea6a3fe69a2a3598a5d20ff0d
Ken Udoudo <kendo302003@gmail.com>
2023-12-31 08:24:09 -0800

    something

--- src/Section7.jsx
+++ src/Section7.jsx
@@ -1,7 +1,7 @@
 const Section7 = () => {
     return (
         <div>
-            <div>
+            <div className="flex flex-col items-center gap-[8px]">
                 <h3>Caring is the new marketing</h3>
                 <p>The Nexcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
             </div>
