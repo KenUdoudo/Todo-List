a0e3fd26bf1df8de4a217a403bd2b46d70657d60
Ken Udoudo <kendo302003@gmail.com>
2024-01-01 08:55:36 -0800

    txt

--- src/Section7.jsx
+++ src/Section7.jsx
@@ -24,7 +24,7 @@ const Section7 = () => {
                 <div className='flex flex-col items-center'>
                     <div><img src={ two }/></div>
                     <div className='flex p-[16px] flex-col items-center gap-[16px] bg-[#F5F7FA] rounded-[8px] shadow-[0px 8px 16px 0px rgba(171, 190, 209, 0.40);] mt-[-96px]'>
-                        <p className='w-[285px] text-center text-[#717171] text-[20px] font-[600]'>Creating Streamlined Safeguarding Processes with OneRen</p>
+                        <p className='w-[285px] text-center text-[#717171] text-[20px] font-[600]'>What are your safeguarding responsibilities and how can you manage them?</p>
                         <div className='flex p-[8px] justify-center items-center gap-[8px] self-stretch'>
                             <p className='text-[#4CAF4F] text-[20px] font-[600]'>Readmore</p>
                             <img src={ arrow }/>
@@ -34,7 +34,7 @@ const Section7 = () => {
                 <div className='flex flex-col items-center'>
                     <div><img src={ three }/></div>
                     <div className='flex p-[16px] flex-col items-center gap-[16px] bg-[#F5F7FA] rounded-[8px] shadow-[0px 8px 16px 0px rgba(171, 190, 209, 0.40);] mt-[-96px]'>
-                        <p className='w-[285px] text-center text-[#717171] text-[20px] font-[600]'>Creating Streamlined Safeguarding Processes with OneRen</p>
+                        <p className='w-[285px] text-center text-[#717171] text-[20px] font-[600]'>Revamping the Membership Model with Triathlon Australia</p>
                         <div className='flex p-[8px] justify-center items-center gap-[8px] self-stretch'>
                             <p className='text-[#4CAF4F] text-[20px] font-[600]'>Readmore</p>
                             <img src={ arrow }/>
