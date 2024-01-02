1f749abb3ac973db50e90660f5730c4150208026
Ken Udoudo <kendo302003@gmail.com>
2024-01-02 09:30:32 -0800

    font

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,13 @@
+import logo from './assests/footerlogo.png'
 const Footer = () => {
     return (
         <div className="font-inter flex px-[165px] py-[64px] items-start gap-[125px bg-[#263238]">
             <div className="flex flex-col items-start gap-[40px]">
-
+                <div><img src={ logo }/></div>
+                <div className='flex flex-col gap-1'>
+                    <p className='text-[16px] text-[#F5F7FA]'>Copyright © 2020 Nexcent ltd.</p>
+                    <p className='text-[16px] text-[#F5F7FA]'>All rights reserved</p>
+                </div>
             </div>
 
             <div>
