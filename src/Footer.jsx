05d139495e6a67d3b759b5d6d28d2e401b8c036c
Ken Udoudo <kendo302003@gmail.com>
2024-01-02 09:38:31 -0800

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,8 @@
 import logo from './assests/footerlogo.png'
+import ig from './assests/ig.png'
+import bball from './assests/bball.png'
+import twitter from './assests/twitter.png'
+import yt from './assests/yt.png'
 const Footer = () => {
     return (
         <div className="font-inter flex px-[165px] py-[64px] items-start gap-[125px bg-[#263238]">
@@ -7,6 +11,12 @@ const Footer = () => {
                 <div className='flex flex-col gap-1'>
                     <p className='text-[16px] text-[#F5F7FA]'>Copyright © 2020 Nexcent ltd.</p>
                     <p className='text-[16px] text-[#F5F7FA]'>All rights reserved</p>
+                </div>
+                <div className='flex items-start gap-4'>
+                    <img src={ ig }/>
+                    <img src={ bball }/>
+                    <img src={ twitter }/>
+                    <img src={ yt }/>
                 </div>
             </div>
 
