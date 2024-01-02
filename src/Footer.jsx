23850ad64bd0e12b590a99d44d6d3eb8a9a84b6d
Ken Udoudo <kendo302003@gmail.com>
2024-01-02 09:47:47 -0800

    text color

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,12 +5,12 @@ import twitter from './assests/twitter.png'
 import yt from './assests/yt.png'
 const Footer = () => {
     return (
-        <div className="font-inter flex px-[165px] py-[64px] items-start gap-[125px bg-[#263238]">
+        <div className="font-inter flex px-[165px] py-[64px] items-start gap-[125px] bg-[#263238] text-[#F5F7FA]">
             <div className="flex flex-col items-start gap-[40px]">
                 <div><img src={ logo }/></div>
                 <div className='flex flex-col gap-1'>
-                    <p className='text-[16px] text-[#F5F7FA]'>Copyright © 2020 Nexcent ltd.</p>
-                    <p className='text-[16px] text-[#F5F7FA]'>All rights reserved</p>
+                    <p className='text-[16px]'>Copyright © 2020 Nexcent ltd.</p>
+                    <p className='text-[16px]'>All rights reserved</p>
                 </div>
                 <div className='flex items-start gap-4'>
                     <img src={ ig }/>
@@ -20,8 +20,19 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div>
-
+            <div className='flex items-start gap-7'>
+                <div className='flex flex-col items-start gap-6'>
+                    <h3>Company</h3>
+                    <div>
+                        <ul>
+                            <li>About us</li>
+                            <li>Blog</li>
+                            <li>Contact us</li>
+                            <li>Pricing</li>
+                            <li>Testimonials</li>
+                        </ul>
+                    </div>
+                </div>
 
             </div>
         </div>
